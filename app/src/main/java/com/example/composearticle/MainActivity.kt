19af64ac796d27f0c3a99d41f13803bacf6f4dad
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-02-19 22:09:44 +0900

    work

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
+import org.intellij.lang.annotations.JdkConstants.TitledBorderJustification
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +39,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageComposeTutorial() {
-    
+fun ComposeArticleWithText(firstText: String, secondText: String, threeText: String) {
+    Column() {
+
+        Text(
+            text = firstText,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(all = 16.dp),
+
+        )
+
+        Text(
+            text = secondText,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+
+
+        )
+
+        Text(
+            text = threeText,
+            modifier = Modifier
+                .padding(all = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun ComposeArticleWithImage(
+    firstText: String, secondText: String, threeText: String,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        ComposeArticleWithText(firstText = stringResource(R.string.first_text),
+            secondText = stringResource(R.string.second_text),
+            threeText = stringResource(R.string.three_text))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeArticleTheme {
-        Greeting("Android")
-    }
+fun ComposeArticlePreview() {
+    ComposeArticleWithImage(
+        stringResource(R.string.first_text), stringResource(
+            R.string.second_text
+        ), stringResource(R.string.three_text)
+    )
+
 }
